a13b3d7fbedddbaefbd85993ea0064c4ca5011ac
Nguyen Hoang Anh Huy <huy.h.nguyen@student.lut.fi>
2023-06-28 21:31:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/softwaredevelopmentskillsmobile2022_23/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package com.example.softwaredevelopmentskillsmobile2022_23;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Who loves you even more than Mommy and Daddy?",
+            "How can you be happy?",
+            "Can anything else make you happy forever?",
+            "How many times can a Catholic get married?",
+            "Who baptizes people in Catholic Church?"
+
+    };
+    public static String choices[][] ={
+            {"Friends","God","Neighbors","Stranger"},
+            {"By loving God","By studying","By playing checkers","By sleeping"},
+            {"No","Yes","I don't know","Maybe"},
+            {"Three times","Twice","Once,unless widowed","Many times"},
+            {"Mommy","Daddy","Uncle","Priests"}
+    };
+
+    public static String correctAnswers[] = {
+            "God",
+            "By loving God",
+            "No",
+            "Once,unless widowed",
+            "Priests"
+    };
+
+}
